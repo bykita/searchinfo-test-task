b8dce93f68486a14e8a8bd2597dbc251b1b808ae
Никита <nikita@MacBook-Pro-Nikita.local>
2023-06-04 16:29:53 +0300

    added project, added list of users, added user page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,69 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="list">
+    <li v-for="user in users" v-bind:key="user.index">
+      <router-link :to="{ path: '/users/' + user.index }">
+        {{ user.name }}
+      </router-link>
+    </li>
   </nav>
-  <router-view/>
+  <div class="main">
+    <router-view/>  
+  </div>
 </template>
+
+<script lang="ts">
+  import json from '@/json/data.json';
+  export default({
+    data() {
+      return {
+        users: json
+      }
+    }
+  })
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
+.main {
+  margin-left: 250px;
+}
+
 nav {
-  padding: 30px;
+  width: 250px;
+  max-height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  position: fixed;
+  overflow-y: auto;
+
+  text-align: left;
 }
 
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  display: block;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.list li {
+	margin-left: 10px;
+	padding: 2px 26px;
+	list-style: none;
+	background-image: url('./assets/user.png');
+	background-repeat: no-repeat;
+	background-position: left center;
+	background-size: 20px;
 }
 </style>
---
+++ src/views/UserView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="users">
+    <h1>{{ getUser.name }}</h1>
+    <div>
+      <img class="users__img" alt="Avatar" :src="require(`../assets/avatars/${getId % 3 + 1}.png`)">
+      <span class="dot" :class="{'dot__green': getUser.isActive, 'dot__red': !getUser.isActive}"></span>
+    </div>
+    <div class="users__info">
+      <p>Компания: {{ getUser.company }}</p>
+      <p>Отдел:    {{ getUser.section }}</p>
+      <p>Позиция:  {{ getUser.position }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import json from '@/json/data.json';
+  export default({
+    computed: {
+      getId() {
+        const id = this.$route.params.id
+        return typeof id === 'string' ? parseInt(id) : parseInt(id.join())
+      },
+      getUser(){
+        const id = this.$route.params.id
+        return typeof id === 'string' ? json[parseInt(id) - 1] : json[parseInt(id.join()) - 1]
+      },
+    }
+  })
+</script>
+
+<style>
+.users__img {
+  max-height: 128px;
+  float: left;
+}
+
+.users__info {
+  padding-top: 1px;
+}
+
+h1 {
+  margin-left: 30px;
+}
+
+.dot {
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  border: 3px solid black;
+  position: absolute;
+  top: 175px;
+  left: 280px;
+}
+
+.dot__red {
+  background-color: crimson;
+}
+
+.dot__green {
+  background-color: darkgreen;
+}
+</style>
